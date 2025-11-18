303f9515c8ed6e0d6f625042d581e2d153ece3f0
Katie Choy <katiec63@nycstudents.net>
2025-11-18 18:53:31 +0000

    .

---
+++ U2T3/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int p = 5;
+int q = 0;
+
+if (p > 0 && (p / q) == 2) {
+  System.out.println("beep");
+} else {
+  System.out.println("boop");
+}
+    }
+}
